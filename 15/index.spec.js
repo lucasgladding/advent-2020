50ab0e2b8b94f0f4414ee42a4e99de992afb39f4
Lucas Gladding <lucas@me.com>
2020-12-15 08:28:49 -0500

    Adds the solution for 15.

---
+++ 15/index.spec.js
@@ -0,0 +1,50 @@
+function update(logs, input, i) {
+    const items = logs[input] || [];
+    logs[input] = [...items.slice(-1), i];
+}
+
+function get(sequence, logs, i) {
+    const last = sequence[i - 1] || 0;
+    const items = logs[last] || [];
+    const length = items.length;
+    if (length < 2) {
+        return 0;
+    }
+    return items[length - 1] - items[length - 2];
+}
+
+function run(start, target) {
+    let last = undefined;
+    const sequence = [];
+    const logs = [];
+    for (let i = 0; i < start.length; i++) {
+        const input = start[i];
+        sequence.push(input);
+        update(logs, input, i);
+        last = input;
+    }
+    for (let j = start.length; j < target; j++) {
+        const input = get(sequence, logs, j);
+        sequence.push(input);
+        update(logs, input, j);
+        last = input;
+    }
+    return last;
+}
+
+describe('part 1', () => {
+    it('gets example 1', () => {
+        const output = run([3, 2, 1], 2020);
+        expect(output).toEqual(438);
+    });
+
+    it('gets example 6', () => {
+        const output = run([3, 1, 2], 2020);
+        expect(output).toEqual(1836);
+    });
+
+    it('gets the input', () => {
+        const output = run([11, 18, 0, 20, 1, 7, 16], 2020);
+        expect(output).toEqual(639);
+    });
+});
