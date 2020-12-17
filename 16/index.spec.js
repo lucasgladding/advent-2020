c721ca31065f262803ff7d7e4042dc47e8501e27
Lucas Gladding <l.gladding@vehikl.com>
2020-12-16 23:38:43 -0500

    Added solution for 16-2.

--- 16/index.spec.js
+++ 16/index.spec.js
@@ -4,6 +4,7 @@ const Field = require('./Field');
 const Ticket = require('./Ticket');
 
 const example_1 = read(__dirname + '/example-1.txt');
+const example_2 = read(__dirname + '/example-2.txt');
 const input = read(__dirname + '/input.txt');
 
 function parse_fields(input) {
@@ -37,5 +38,77 @@ describe('part 1', () => {
     const errors = [];
     tickets.filter(t => t.test(fields, errors));
     expect(sum(errors)).toEqual(27870);
+  });
+});
+
+function map_field(ticket, fields) {
+  return ticket.values.map((v) => {
+    return fields.filter(f => f.test(v)).map(f => f.name);
+  });
+}
+
+function intersect(a, b) {
+  return a.filter(v => b.includes(v));
+}
+
+function intersect_field(matches) {
+  return matches.slice(1).reduce((output, current, j) => {
+    return output.map((field, i) => {
+      return intersect(field, current[i]);
+    });
+  }, matches[0]);
+}
+
+function flatten(input) {
+  return input.reduce((output, i) => {
+    return [...output, ...i]
+  }, []);
+}
+
+function get_unique_field(input) {
+  const found = flatten(input.filter(i => i.length === 1));
+  if (found.length === input.length) {
+    return input;
+  }
+  const remaining = input.map((i) => {
+    if (i.length === 1) {
+      return i;
+    }
+    return i.filter(i => !found.includes(i));
+  });
+  return get_unique_field(remaining);
+}
+
+describe('part 2', () => {
+  it('calculates the order for example 2', () => {
+    const sections = example_2.split('\n\n');
+    const fields = parse_fields(sections[0]);
+    const tickets = parse_tickets(sections[2]);
+    const matches = tickets.map(t => map_field(t, fields));
+    const names = flatten(get_unique_field(intersect_field(matches)));
+    console.log('debug', names);
+  });
+
+  function get_product(names, ticket) {
+    const indexes = names.reduce((output, name, i) => {
+      if (name.includes('departure')) {
+        output.push(i);
+      }
+      return output;
+    }, []);
+    return indexes.reduce((output, i) => {
+      return output * ticket.values[i];
+    }, 1);
+  }
+
+  it('calculates the order for input', () => {
+    const sections = input.split('\n\n');
+    const fields = parse_fields(sections[0]);
+    const mine = parse_tickets(sections[1])[0];
+    const tickets = parse_tickets(sections[2]);
+    const matches = tickets.filter(t => t.test(fields)).map(t => map_field(t, fields));
+    const names = flatten(get_unique_field(intersect_field(matches)));
+    const product = get_product(names, mine);
+    expect(product).toEqual(3173135507987);
   });
 });
